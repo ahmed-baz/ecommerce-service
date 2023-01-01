c61a5a547b82f43c2d3f71c1d19a57c421e38bc8
ahmed.baz <ahmed.baz@fawry.com>
2023-01-01 23:18:07 +0200

    add springdoc and springFox config

---
+++ src/main/java/com/skyros/ecommerce/config/SpringFoxConfig.java
@@ -0,0 +1,34 @@
+package com.skyros.ecommerce.config;
+
+
+public class SpringFoxConfig {
+
+    /*
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Ahmed Baz", "https://facebook.com/ahmedbaz1024/", "ahmed.baz@fawry.com");
+        Collection<VendorExtension> vendorExtensions = new HashSet<>();
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Online E-Commerce App",
+                "1.0.0",
+                "Terms of service",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                vendorExtensions);
+        return apiInfo;
+    }
+     */
+
+}
+
