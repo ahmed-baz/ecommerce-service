ff7d0194b0feb06d3a48da02748292435fcf8f5d
ahmed.baz <ahmed.baz@fawry.com>
2023-01-07 17:02:30 +0200

    update pom file and EcommerceAppApplication

--- src/main/java/com/skyros/ecommerce/EcommerceAppApplication.java
+++ src/main/java/com/skyros/ecommerce/EcommerceAppApplication.java
@@ -2,9 +2,10 @@ package com.skyros.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceAppApplication {
+public class EcommerceAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceAppApplication.class, args);
